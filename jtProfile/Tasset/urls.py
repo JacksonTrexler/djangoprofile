af6f5bf223703f1ed9cd6f3aaff7ceead193a187
Jackson <jtrexler25@gmail.com>
2025-06-21 16:23:10 -0400

    url fix

--- jtProfile/Tasset/urls.py
+++ jtProfile/Tasset/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from Tasset import views
 
 urlpatterns = [
-    path("", include("Tasset.urls")),
-    path('admin/', admin.site.urls)
+    path("", views.home, name="home"),
     
     #path("", views.home, name="home"),
     #path("map/", views.map, name="map"),
--- jtProfile/Tasset/views.py
+++ jtProfile/Tasset/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Tasset!")
+    return HttpResponse("<html style=\"color:red;\">Get ready for Tasset!</html>")
